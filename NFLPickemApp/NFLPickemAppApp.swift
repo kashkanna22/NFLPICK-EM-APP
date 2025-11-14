5273269d085c85e752631a104c490359d3ac7a27
Kashyap Kannajyosula <kashkanna22@gmail.com>
2025-11-14 12:30:35 -0500

    Initial Commit

---
+++ NFLPickemApp/NFLPickemAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFLPickemAppApp.swift
+//  NFLPickemApp
+//
+//  Created by Kashyap Kannajyosula on 11/14/25.
+//
+
+import SwiftUI
+
+@main
+struct NFLPickemAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
